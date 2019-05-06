4e854c0795a1b508096d097e054cedccb8b8ad9e
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-06 11:39:55 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,12 +1,14 @@
 module ArtistsHelper
 
 	def display_artist(new_song)
-		if new_song.artist
+		if !new_song.artist
+			link_to "Add Artist", edit_song_path(new_song)
 			#artist existing associated with a song 
-			link_to new_song.artist.name, artist_path(new_song.artist) 
+			#link_to new_song.artist.name, artist_path(new_song.artist) 
 		else
+			link_to new_song.artist.name, artist_path(new_song.artist) 
 			#link to artist if artist  is not associated wit a song
-		link_to "Add Artist", edit_song_path(new_song)
+		#link_to "Add Artist", edit_song_path(new_song)
 	end
 		
 	end
