c3d6cac3014ebe87367a6da02b8dfa4c84b1568f
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-06 11:33:35 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,10 @@
 module ArtistsHelper
 
-	def display_artist(song)
-		if song.artist
-			link_to song.artist.name, artist_path(song.artist) 
+	def display_artist(new_song)
+		if new_song.artist
+			link_to new_song.artist.name, artist_path(new_song.artist) 
 		else
-		link_to "Add Artist", edit_song_path(song)
+		link_to "Add Artist", edit_song_path(new_song)
 	end
 		
 	end
